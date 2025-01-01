ee1a5e3365993258b3c4284e9047000c4b0d1629
yashtyagi298 <tyagiyash097@gmail.com>
2025-01-01 23:45:44 +0530

    add some changes

--- client/src/components/RecipeListing.jsx
+++ client/src/components/RecipeListing.jsx
@@ -9,8 +9,7 @@ const RecipeListing = ({ secondary }) => {
 				Simple and tasy Categories
 			</h3>
 			<p className='max-w-3xl mx-auto text-dark-60 text-center'>
-				Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqut enim ad minim
+			We bring you the taste of home at an affordable price, offering a variety of delicious options in tiffin thali, breakfast, lunch, snacks, and sweets, making every bite feel like comfort and care!
 			</p>
 			<div className='flex justify-between xl:justify-center xl:gap-x-[3%] flex-wrap mt-10'>
 				{recipes.primary.map(recipe => {
