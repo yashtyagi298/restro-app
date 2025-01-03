e8903d8ab3a2403f4360050d55687b018f88d80e
yashtyagi298 <tyagiyash097@gmail.com>
2025-01-03 19:57:53 +0530

    recipe chng

--- client/src/data/recipes.js
+++ client/src/data/recipes.js
@@ -26,7 +26,7 @@ export const recipes = {
 			title: 'Fresh Matar Paneer and Bhujia with salad ',
 			imageUrl: 'TiffenPanner.png',
 			time: 25,
-			type: 'Tiffen',
+			type: 'Tiffin',
 		},
 		{
 			id: 5,
@@ -57,62 +57,5 @@ export const recipes = {
 			type: 'Dinner',
 		},
 	],
-	secondary: [
-		{
-			id: 9,
-			title: 'Mixed Tropical Fruit Salad with Superfood Boosts ',
-			imageUrl: '9.png',
-			time: 25,
-			type: 'Healthy',
-		},
-		{
-			id: 10,
-			title: 'Big and Juicy Wagyu Beef Cheeseburger',
-			imageUrl: '10.png',
-			time: 30,
-			type: 'Western',
-		},
-		{
-			id: 11,
-			title: 'Healthy Japanese Fried Rice with Asparagus',
-			imageUrl: '11.png',
-			time: 20,
-			type: 'Healthy',
-		},
-		{
-			id: 12,
-			title: 'Cauliflower Walnut Vegetarian Taco Meat',
-			imageUrl: '12.png',
-			time: 25,
-			type: 'Eastern',
-		},
-		{
-			id: 13,
-			title: 'Rainbow Chicken Salad with Almond Honey Mustard Dressing',
-			imageUrl: '13.png',
-			time: 40,
-			type: 'Healthy',
-		},
-		{
-			id: 14,
-			title: 'Barbeque Spicy Sandwiches with Chips ',
-			imageUrl: '14.png',
-			time: 15,
-			type: 'Snack',
-		},
-		{
-			id: 15,
-			title: 'Firecracker Vegan Lettuce Wraps - Spicy! ',
-			imageUrl: '15.png',
-			time: 30,
-			type: 'Seafood',
-		},
-		{
-			id: 16,
-			title: 'Chicken Ramen Soup with Mushroom ',
-			imageUrl: '16.png',
-			time: 25,
-			type: 'Japanese',
-		},
-	],
+	
 };
